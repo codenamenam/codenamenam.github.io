041d17e8cfab18efcdb81e779ec97466a0fc0c63
codenamenam <bluetire27@gmail.com>
2023-06-01 01:18:21 +0900

    SADfs

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-var categories = [];
-
 function toggleDetails(category) {
-  var categoryContents = document.getElementById(category);
-  categoryContents.classList.toggle("show");
+  // 모든 카테고리 내용을 감춥니다
+  var categoryContents = document.querySelectorAll("#category-contents > div");
+  categoryContents.forEach(function (content) {
+    content.style.display = "none";
+  });
+
+  // 선택한 카테고리에 해당하는 내용을 보여줍니다
+  var selectedContent = document.getElementById(category);
+  selectedContent.style.display = "block";
 }
+
+/*
+  var categories = document.getElementById("category-box");
+  var categor = categories.getElementById(category);
+  categor.style.background = "#2C98F0";
+  */
